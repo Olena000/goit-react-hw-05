42ad136b0ec0d55c1cae387b582214810831105c
Olena000 <154604010+Olena000@users.noreply.github.com>
2024-08-02 11:21:42 +0300

    vercel

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function NotFoundPage() {
   return (
     <div>
-      <p>Sorry... 404</p>
-      <Link to={"/"}>Back to home</Link>
+      <p>Not found... 404</p>
+      <Link to="/">Back to home</Link>
     </div>
   );
 }
